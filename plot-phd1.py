e3a0d56b207c4dfb9e46469cf856d69380df0e77
duddles <duddles@users.noreply.github.com>
2022-10-16 09:30:32 -0500

    Update plot-phd1.py
    Filled in plot_phd()

--- plot-phd1.py
+++ plot-phd1.py
@@ -44,9 +44,29 @@ def plot_phd1(df, name, markersize=2):
     name: file name
     markersize: size of dots in plot
     """
-    
-    # To do: create this function to plot the phd quality scores
-    # save it as a file
+    # color the points based on the y-value
+    # I used this conditions/choices method from this youtube talk
+    # at 12:30 into talk
+    # https://www.youtube.com/watch?v=nxWginnBklU
+    # note that if multiple conditions are met - the first one is used, so
+    # if > 50 that will be used and not the others
+    conditions = [
+        df.v1 > 50, 
+        df.v1 > 40,
+        df.v1 > 30,
+        df.v1 > 20,
+        df.v1 > 10, 
+    ]
+    choices = ["black", "purple", "blue", "green", "orange"]
+
+    df["colors"] = np.select(conditions, choices, default="red")
+    fig, ax = plt.subplots()
+    yvals = df.v1.values
+    xvals = range(len(yvals))
+    colors = df.colors
+    ax.scatter(xvals, yvals, s=markersize, color=colors)
+    ax.set_title(name)
+    ax.set_ylim(0,65)
 
 p = Path(".")
 
